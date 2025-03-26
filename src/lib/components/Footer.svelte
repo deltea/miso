bc4e9355b0b2a4f0c709733ae2c78ed6e83ac7bf
deltea <thcheetah777@gmail.com>
2025-03-25 21:13:39 -0700

    add more padding to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="flex justify-between items-center h-10 w-full px-2 font-bold">
+<footer class="flex justify-between items-center h-10 w-full px-4 font-bold">
   <p>made with svelte</p>
 
   <a href="https://github.com/deltea/miso">github</a>
